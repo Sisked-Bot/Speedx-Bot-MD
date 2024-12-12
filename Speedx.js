49d5b019b7c3d500ef9cb6f34186b8eeb63af5bf
Sisked <anuarmazenett@gmail.com>
2024-12-12 16:32:49 -0400

    Update and rename starlights.js to Speedx.js

--- Speedx.js
+++ Speedx.js
@@ -113,7 +113,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? ['Sumi Sakurasawa', 'Safari', '2.0.0'] : methodCodeQR ? ['Sumi Sakurasawa', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '110.0.5585.95'],
+browser: opcion == '1' ? ['Sisked Bot', 'Safari', '2.0.0'] : methodCodeQR ? ['Sisked Bot', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '110.0.5585.95'],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
