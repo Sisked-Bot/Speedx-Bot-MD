1b54baa125d45a0871be1273a9ace5dee103244a
Sisked <anuarmazenett@gmail.com>
2024-12-26 16:08:23 -0400

    Update main-infobot.js

--- plugins/main-infobot.js
+++ plugins/main-infobot.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, isRowner}) => {
 	let _muptime
 	let totalreg = Object.keys(global.db.data.users).length
 	let totalchats = Object.keys(global.db.data.chats).length
-	let pp = 'https://tinyurl.com/26joweab'
+	let pp = 'https://tinyurl.com/mw7436as'
     if (process.send) {
       process.send('uptime')
       _muptime = await new Promise(resolve => {
@@ -15,7 +15,7 @@ let handler = async (m, { conn, isRowner}) => {
   const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
   const used = process.memoryUsage()
   let txt = `╭─⬣「 *Info Bot* 」⬣\n`
-      txt += `│  ≡◦ *🏳️ Creador ∙* Sisked\n`
+      txt += `│  ≡◦ *🏳️ Creador ∙* Onyx\n`
       txt += `│  ≡◦ *📚 Grupos Unidos ∙* ${groupsIn.length}\n`
       txt += `│  ≡◦ *👤 Chats Privados ∙* ${chats.length - groupsIn.length}\n`
       txt += `│  ≡◦ *💬 Total De Chats ∙* ${chats.length}\n`
